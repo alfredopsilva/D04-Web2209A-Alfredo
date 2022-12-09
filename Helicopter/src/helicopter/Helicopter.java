0a84f7e0508a7245e96a0869f9462842e4de0af6
Alfredo <a.alfredops@gmail.com>
2022-12-09 15:29:59 -0500

    Helicopter 1

---
+++ Helicopter/src/helicopter/Helicopter.java
@@ -0,0 +1,43 @@
+package helicopter;
+
+public class Helicopter {
+
+     private static int nextId;
+     private int id;
+     private int fuelLevel ;
+    private int altitude ;
+private boolean engineRunning = false;
+
+    // Constructor
+
+    public Helicopter(){
+        this.id = nextId++;
+        this.fuelLevel = 100;
+        this.altitude = 0;
+        this.engineRunning = false;
+    };
+
+
+    // Getters
+    public int getId(){
+        return id;
+    }
+    public int getFuelLevel() {
+        return fuelLevel;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public boolean isEngineRunning() {
+        return engineRunning;
+    }
+
+    @Override
+    public String toString(){
+        return "Helicopter #" + id;
+    }
+}
+
+
---
+++ Helicopter/src/helicopter/Program.java
@@ -0,0 +1,9 @@
+package helicopter;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+    }
+
+}
