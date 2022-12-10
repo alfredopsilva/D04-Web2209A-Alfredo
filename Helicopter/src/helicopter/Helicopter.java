64ea5b2b3e468f06e6b7a734157986d75de6e8a6
Alfredo <a.alfredops@gmail.com>
2022-12-10 11:46:13 -0500

    Working

--- Helicopter/src/helicopter/Helicopter.java
+++ Helicopter/src/helicopter/Helicopter.java
@@ -2,11 +2,12 @@ package helicopter;
 
 public class Helicopter {
 
+    //Private Data-Members
      private static int nextId = 82;
      private String id = "HEP12";
      private int fuelLevel ;
-    private int altitude ;
-private boolean engineRunning = false;
+     private int altitude ;
+     private boolean engineRunning = false;
 
     // Constructor
 
@@ -17,26 +18,84 @@ private boolean engineRunning = false;
         this.engineRunning = false;
     };
 
-
     // Getters
-    public String getId(){
+    public String getId()
+    {
         return id;
     }
-    public int getFuelLevel() {
+    public int getFuelLevel()
+    {
         return fuelLevel;
     }
-
-    public int getAltitude() {
+    public int getAltitude()
+    {
         return altitude;
     }
-
-    public boolean isEngineRunning() {
+    public boolean isEngineRunning()
+    {
         return engineRunning;
     }
 
+    public boolean isFuelEmpty()
+    {
+        return getFuelLevel() == 0;
+    }
+
+    public boolean isFuelFull()
+    {
+        return getFuelLevel() == 100;
+    }
+
+    public boolean isLanded()
+    {
+        return getAltitude() == 0;
+    }
+
+    public boolean isFlying()
+    {
+        return getAltitude() != 0;
+    }
+
+    // Can-Do-Action Getters
+    public boolean canStartEngine()
+    {
+        return !engineRunning
+                && isLanded()
+                && !isFuelEmpty();
+    }
+
+    public void startEngine(){
+        if(canStartEngine())
+        {
+            engineRunning = true;
+        }
+    }
+
+    public boolean canStopEngine()
+    {
+        return engineRunning;
+    }
+
+    public void stopEngine()
+    {
+        if(canStopEngine())
+            engineRunning = false;
+    }
+
+    // To String method
     @Override
-    public String toString(){
+    public String toString()
+    {
         return "Helicopter #" + id;
+    }
+
+    public String displayHelicopter() // Why this method is not Override?
+    { 
+        return toString()
+               + ": " + (isEngineRunning() ? "Engine on" : "Engine off")
+               + ", " + "Altitude = " + altitude
+               + ", " + "Fuel Level = " + fuelLevel;
+
     }
 }
 
--- Helicopter/src/helicopter/HelicopterMenu.java
+++ Helicopter/src/helicopter/HelicopterMenu.java
@@ -1,38 +1,54 @@
 package helicopter;
 
 import utility.console.ConsoleMenu;
-
 import java.util.ArrayList;
 
 public class HelicopterMenu {
-
+    
+    //Private Data Members
     private final Helicopter helicopter;
     private final ConsoleMenu menu;
 
-
+    // Helicopter Menu Constructor
     public HelicopterMenu(Helicopter helicopter){
+       
         this.helicopter = helicopter;
+      
+        //Adding Options for Menu
         ArrayList<String> options = new ArrayList<>();
         options.add("> Start");
         options.add("> Stop");
         options.add("> Display");
         options.add("> Exit");
 
+        //Creating Menu List
         menu = new ConsoleMenu("MENU", "Select an Option above : ", options);
     };
 
-    public void start (){
+    public void start () {
 
-        while(true) {
+        System.out.println(helicopter.displayHelicopter());
 
+        while (true) {
             int selection = menu.displayAndGetSelection();
-            if (selection == 3) {
-                System.out.println(helicopter.toString());
-            } else if (selection == menu.getSize()) {
+
+            if (selection == 1) {
+
+                helicopter.startEngine();
+            } 
+            else if (selection == 2)
+            {
+                helicopter.stopEngine();
+
+            } else if (selection == 3) 
+            {
+                System.out.println(helicopter.displayHelicopter());
+
+            } else if (selection == 4) {
+                
                 return;
+                }
             }
-        }
-    };
-
-
+        
+    }
 }
