7a2bbfe0768d9d781ce11c4be5b10b014a79ddcb
Alfredo <a.alfredops@gmail.com>
2022-12-16 10:33:27 -0500

    Helicopter 5 | Finished

--- Helicopter/src/helicopter/AirportMenu.java
+++ Helicopter/src/helicopter/AirportMenu.java
@@ -38,8 +38,8 @@ public class AirportMenu {
             switch(selection) {
                 case 1: addHelicopter();          break;
                 case 2: removeHelicopter();       break;
-                case 3: ;            break;
-                case 4: ;              break;
+                case 3: controlHelicopter();      break;
+                case 4: landAllHelicopter();      break;
                 case 5: ;          break;
                 case 6: return;
             }
@@ -62,18 +62,43 @@ public class AirportMenu {
 
         ConsoleMenu HelicopterMenu = new ConsoleMenu("Remove an Helicopter", "Select One Helicopter:", options);
         int index = HelicopterMenu.displayAndGetSelection() - 1;
+        if(index < helicopters.size()){
+            helicopters.remove(index);
+        }
+        else if(index == helicopters.size() + 1)
+        {
+            return;
+        }
     }
-
-
 
     public void controlHelicopter()
     {
+        ArrayList<String> options = new ArrayList<String>();
+        for(Helicopter helicopter : helicopters) {
+            options.add(helicopter.toString());
+        }
+        options.add("Exit");
+
+        ConsoleMenu HelicopterMenu = new ConsoleMenu("Control an Helicopter", "Select One Helicopter:", options);
+        int index = HelicopterMenu.displayAndGetSelection() - 1;
+        if(index < helicopters.size()){
+            HelicopterMenu menu = new HelicopterMenu(helicopters.get(index));
+            menu.start();
+        }
+        else if(index == helicopters.size() + 1)
+        {
+            return;
+        }
+
 
     }
 
     public void landAllHelicopter()
     {
-
+        for(Helicopter helicopter : helicopters)
+        {
+            helicopter.landing();
+        }
     }
 
     public void displayAllHelicopters()
